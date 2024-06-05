97fba1ff3efa6bb235bd450d28506d87c7e4505f
pabvidort <pabvidort@ORN1-10338220>
2024-06-05 15:31:21 +0200

    Probando a subir mis cosas al repositorio local

---
+++ src/probandorefactorizacion/ProbandoRefactorizacion.java
@@ -0,0 +1,27 @@
+
+package probandorefactorizacion;
+
+class coche extends SuperCoche{
+    public coche(int matricula, String marca) {
+        this.matricula = matricula;
+        this.marca = marca;
+    }
+    
+}
+
+
+
+public class ProbandoRefactorizacion {
+    public static int sumar(int a,int b){
+        return a + b;
+    }
+    public static void main(String[] args) {
+       int nuevoNombreNumero = 5;
+               int suma = nuevoNombreNumero + 10;
+       int resto = nuevoNombreNumero + suma;
+       int divisor = nuevoNombreNumero - resto;
+       int numero1 = 4;
+       int numero2 = 7;
+    }
+    
+}
---
+++ src/probandorefactorizacion/SuperCoche.java
@@ -0,0 +1,13 @@
+
+package probandorefactorizacion;
+
+public class SuperCoche {
+    
+    int matricula;
+    String marca;
+    int anio;
+
+    public SuperCoche() {
+    }
+    
+}
